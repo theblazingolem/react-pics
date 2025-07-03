f306f449bae2c36f34da4391bb119c746a8f216c
Blazing <160132339+theblazingolem@users.noreply.github.com>
2025-07-03 07:48:08 +0530

    upload files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import SearchBar from "./components/SearchBar";
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <SearchBar />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+import "./SearchBar.css";
+import srchLogo from "../assets/srch.svg";
+
+export default function SearchBar() {
+    return (
+        <>
+            <input type="text"></input>
+            {/* <img src="../assets/srch.svg"></img> */}
+        </>
+    );
+}
