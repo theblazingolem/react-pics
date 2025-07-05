2188c564f904254f75e115441b2a5d1185c2792d
Blazing <160132339+theblazingolem@users.noreply.github.com>
2025-07-05 10:36:19 +0530

    added srch functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,23 @@
+import { useEffect } from "react";
 import "./SearchBar.css";
 import srchLogo from "../assets/search.svg";
 
 export default function SearchBar() {
+    useEffect(() => {
+        const srchBtn = document.getElementById("srchBtn");
+        const srchInp = document.getElementById("srchInp");
+        const srch = () => {
+            console.log(srchInp.value);
+        };
+        srchBtn.addEventListener("click", srch);
+        return () => {
+            srchBtn?.removeEventListener("click", srch); // 🧼 Clean up
+        };
+    }, []);
     return (
         <div className="srch-bar">
-            <input type="text"></input>
-            <img src={srchLogo} alt="" />
+            <input id="srchInp" type="text"></input>
+            <img id="srchBtn" src={srchLogo} alt="" />
         </div>
     );
 }
